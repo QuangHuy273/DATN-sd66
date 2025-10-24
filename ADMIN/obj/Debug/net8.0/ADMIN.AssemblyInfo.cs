2fc393733bd05a93492a52df5f476b5720ee0471
trananhzxc <trananh17022004@gmail.com>
2025-10-24 10:47:18 +0700

    update controller

--- ADMIN/obj/Debug/net8.0/ADMIN.AssemblyInfo.cs
+++ ADMIN/obj/Debug/net8.0/ADMIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADMIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b8b5f6c3e0f58bd0eed2e4a2b90e42989cf282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3e30da886b9db9d503ae5e5308e9fae3bf71e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADMIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADMIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
